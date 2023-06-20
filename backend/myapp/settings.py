183d368bebe23ebd54e19a59ce6893ec30479721
明石凜太 <jeffrey.bezos@icloud.com>
2023-06-21 00:30:52 +0900

    環境構築完了
    Nextjs
    TailWind
    Django
    TypeScript

--- backend/myapp/settings.py
+++ backend/myapp/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ls)su-v6i2knhi(xi)0y0px50)d&z30@90^^5r1yss=*f8-k*'
+SECRET_KEY = 'django-insecure-k&(5847b&-s+$4=bo)1cw5)=d6(&nsz-i!ys2a8nexej&vww2i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
